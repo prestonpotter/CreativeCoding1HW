2812f8d18674ddb4982a7e704896b730589030f8
prestonpotter <prestonpotter1711@gmail.com>
2025-03-30 23:59:01 -0600

    setup

---
+++ Week 9/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(500, 600);
+  }
+  
+  function draw() {
+    background(220);
+    
+      //neck
+    fill(255, 211, 153);
+    rect(220,155,60,65)
+    
+    //head
+  fill(255, 211, 153);
+  circle(250,125,125);
+    
+    //hair
+    fill(31, 26, 20)
+    line(35, 155, 155, 155)
+  }
