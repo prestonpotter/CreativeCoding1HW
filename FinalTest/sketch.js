930a0b8a6ca68877008081bbee357006f479758f
prestonpotter <prestonpotter1711@gmail.com>
2025-05-02 12:56:52 -0600

    UpdatedCode

--- FinalTest/sketch.js
+++ FinalTest/sketch.js
@@ -14,16 +14,43 @@ let S1, S2;
 let S1X= 450;
 let S1Y= 400;
 
+let gameOver = false;
+let bg;
+
+
+let currentImageT;
+let currentImageS;
+
 
 function setup() {
   createCanvas(600, 600);
   currentImageT=T1;
   currentImageS=S1
+  resetButtonWidth = 100;
+  resetButtonHeight = 50;
+  resetButtonX = (width - resetButtonWidth) / 2;
+  resetButtonY = height - 70;
 }
 
 function draw() {
 
+  if(gameOver) {
+    background(bg);
+
+    image(Reset, resetButtonX, resetButtonY, resetButtonWidth, resetButtonHeight);
+
+    if (currentImageT === TH) {  
+    image(SW, 150, 150, 300, 300);
+  } else if(currentImageS === SH) {
+    image(TW, 150, 150, 300, 300);
+  }
+    return;
+  }
+
+
   background(bg);
+  image(Left, 10, 10, 115, 115)
+  image(Right, 490, 8, 115, 115)
   image(currentImageT, T1X, T1Y,90,90)
   image(currentImageS, S1X,S1Y,90,90)
   TMovement();
@@ -41,6 +68,9 @@ function draw() {
       console.log("Hit Thrower")
       snowballs.splice(i,1);
       currentImageT = TH;
+      setTimeout(() => {
+        gameOver = true;
+      }, 1000);
   
   }
   }
@@ -51,27 +81,58 @@ function draw() {
       console.log("Hit Snowman")
       snowballs.splice(i, 1);
       currentImageS = SH;
-        }
+      setTimeout(() => {
+         gameOver = true;
+        }, 1000);
 
   }
-}}
+}
+}
+}
+
+function mousePressed() {
+  if (gameOver) {
+    if (mouseX > resetButtonX && mouseX < resetButtonX + resetButtonWidth &&
+        mouseY > resetButtonY && mouseY < resetButtonY + resetButtonHeight) {
+      // Reset the game state
+      gameOver = false;
+      snowballs = [];
+      currentImageT = T1;
+      currentImageS = S1;
+      T1X = 50;
+      T1Y = 400;
+      S1X = 450;
+      S1Y = 400;
+    }
+  }
+}
+
+let resetButtonX, resetButtonY, resetButtonWidth, resetButtonHeight;
 
 
 function preload()
 {
   T1=loadImage('thrower1.png')
   T2=loadImage('thrower2.png')
-  bg=loadImage('background.png')
+  bg=loadImage('snowbackground.png')
   S1=loadImage('snowman1.png')
   S2=loadImage('snowman2.png')
   TH=loadImage('throwerhit.png')
   SH=loadImage('snowmanhit.png')
+  TW=loadImage('ThrowerWIn.png')
+  SW=loadImage('SnowmanWin.png')
+  Reset=loadImage('resetbutton.png')
+  Left=loadImage('TI.png')
+  Right=loadImage('SM.png')
 }
+
+
 
 
 //throwmotion t
   function keyPressed()
   {
+    if(gameOver) return;
   if (keyCode === 32) {
     currentImageT = T2;
     throwSnowball(T1X + 70, T1Y+30, 1, "thrower");
@@ -109,43 +170,56 @@ function TMovement() {
 //keys
   if(keyIsDown(w))
   {
+    if (T1Y > 0) {
     T1Y -= 10;
   }
+}
 else if(keyIsDown(a))
   {
+    if (T1X > 0) {
     T1X -= 10;
+    }
     console.log("movement:" + T1X);
   }
 else if(keyIsDown(s))
   {
+    if (T1Y < height - 90) {
     T1Y += 10;
   }
+}
 else if(keyIsDown(d))
   {
+    if (T1X < width / 2 - 90) {
     T1X += 10;
   }
+  }
 }
+  
+
   //snowmanmovement
-  function SMovement()
+  function SMovement() {
 
 //keys
-  {
-    if(keyIsDown(73))
-  {
+  
+    if(keyIsDown(73)){
+    if (S1Y > 0) {
     S1Y -= 10;
   }
-else if(keyIsDown(74))
-  {
+}
+ if(keyIsDown(74)){
+    if (S1X > width / 2) {
     S1X -= 10;
-    console.log("movement:" + T1X);
+    }
+    console.log("Snowman X Position:" + S1X);
   }
-else if(keyIsDown(75))
-  {
+if(keyIsDown(75)){
+    if (S1Y < height - 90) {
     S1Y += 10;
+    }
   }
-else if(keyIsDown(76))
-  {
+ if(keyIsDown(76)){
+    if (S1X < width - 90) {
     S1X += 10;
   }
-  
-  }
+}
+}
